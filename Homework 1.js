a697fc9dd2563fb3d9d6ddfe334762064e5a932a
Hasmik-Avetisyan <85196516+Hasmik-Avetisyan@users.noreply.github.com>
2021-06-12 17:29:20 +0400

    Add files via upload

---
+++ Homework 1.js
@@ -0,0 +1,33 @@
+//Xndir 1
+let N = 55
+if (N % 2 === 0) {
+    console.log('even')
+} else {
+    console.log('odd')
+}
+
+//Xndir 2
+let Number = 367
+const LD = Number%10
+const MD = ((Number - LD)%100)/10
+const FD = (((Number - LD)/10-MD)/10)%10
+    console.log(LD,MD,FD)
+
+    //Xndir 3
+let a = -3
+let b = 9
+let c = 16
+let d = 2
+let e = -7
+let average = ((a+b+c+d+e)/5)
+    console.log(average)
+
+//Xndir 4
+let n =10
+if (n<15) {
+    console.log("45,26,78")
+ } else if (n>=15 && n<30) {
+      console.log("26,45,78")
+ } else {
+    console.log("78,29,45")
+ }
