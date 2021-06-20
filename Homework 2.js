bc03353e863099b45f8ef7c6ece7254b4f3acf1d
Hasmik-Avetisyan <85196516+Hasmik-Avetisyan@users.noreply.github.com>
2021-06-20 19:42:15 +0400

    Add files via upload

---
+++ Homework 2.js
@@ -0,0 +1,17 @@
+//xndir 4
+let array = [1, 2, 3, 4, 5, 6, 7];
+console.log (array.reverse ());
+
+//xndir 2
+console.log(Math.pow(7,45))
+
+//xndir1
+let num = +prompt()
+let count
+for(let i=0; i<num.length; i++) {
+    if(num===9){
+console.log(count)
+}
+}
+
+// Myus xndirnery chem karoxacel lucel
